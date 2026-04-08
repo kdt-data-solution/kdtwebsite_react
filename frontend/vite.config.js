1b464a81edf0827304197aeaee1e22f884e5c830
Leah <148556021+Lbarajon@users.noreply.github.com>
2026-04-08 16:43:34 +0800

    04-09

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/kdtwebsite_react/',
   plugins: [tailwindcss()],
   root: 'src',
   build: {
